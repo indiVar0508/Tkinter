be3a11b4c6e277ae97dabc55228324dcd50f4637
indivar mishra <indivar.mishra@highradius.com>
2020-03-06 22:29:33 +0530

    Second Commit

--- main.py
+++ main.py
@@ -13,18 +13,17 @@ class SeaofBTCapp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
 
         self.frames = {}
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
+        for f in (StartPage, PageOne, PageTwo):
+            frame = f(container, self)
+            self.frames[f] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
 
-        frame.grid(row=0, column=0, sticky='nsew')
         self.show_grid(StartPage)
 
     def show_grid(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
-def qf(x):
-    print(x)
 
 class StartPage(tk.Frame):
 
@@ -32,9 +31,32 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text='Start Page', font=LARGE_FONT)
         label.pack(pady=10, padx=10)
-
-        button1 = tk.Button(self, text='click here', command=lambda: qf('hello'))
+        button1 = tk.Button(self, text='next', command=lambda: controller.show_grid(PageOne))
         button1.pack()
+        button2 = tk.Button(self, text='last', command=lambda: controller.show_grid(PageTwo))
+        button2.pack()
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text='PageOne', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text='back', command=lambda: controller.show_grid(StartPage))
+        button1.pack()
+        button2 = tk.Button(self, text='next', command=lambda: controller.show_grid(PageTwo))
+        button2.pack()
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text='PageTwo', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text='Start', command=lambda: controller.show_grid(StartPage))
+        button1.pack()
+        button2 = tk.Button(self, text='Back', command=lambda: controller.show_grid(PageOne))
+        button2.pack()
 
 app = SeaofBTCapp()
 app.mainloop()
